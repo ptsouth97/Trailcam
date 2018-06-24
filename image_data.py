6233e1e7182729c89733133bd851a6b05bb8f81a
Blake Crosby <ptsouth97@gmail.com>
2018-06-24 12:09:45 -0400

    changed image_data.py to use exif data as list not string

--- image_data.py
+++ image_data.py
@@ -27,10 +27,21 @@ def getexif(image, d_row):
 	metadata_str = info.get(270)
 	print(metadata_str)
 
-	obs_time = metadata_str[0:11]
+	# Split the string on the colons to make a list
+	metadata_list = metadata_str.split(':')
+	print(metadata_list)
+	
+	obs_time = metadata_list[0] + ':' + metadata_list[1]
+	temp_with_units = metadata_list[2]
+	temp = temp_with_units[:-1]
+	camera = metadata_list[4]
+	moon_info = metadata_list[5]	
+	moon = moon_info[0:2]
+
+	'''obs_time = metadata_str[0:11]
 	temp = metadata_str[12:14]
 	camera = metadata_str[21:28]
-	moon = metadata_str[29:31]
+	moon = metadata_str[29:31]'''
 
 	print(camera)
 
