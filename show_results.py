f749cf6f74daacabcb03d78fcdeedcf6de65f4c3
ptsouth97 <ptsouth97@gmail.com>
2018-09-14 22:31:11 -0400

    added exit option

--- show_results.py
+++ show_results.py
@@ -21,26 +21,32 @@ def main():
 	columns = ['obs_time', 'temp', 'moon', 'stand', 'deer', 'bucks', 'does', 'hogs', 'dark', 'day_bucks', 
 				'day_deer', 'day_does', 'day_hogs', 'light']
 
-	print('CHOICES:')
-	print('(1) View deer by moonphase')
-	print('(2) View deer by temperature')
-	choice = input('What do you want to do? ').strip()
 
-	if choice == '1':
-		stmt = stmt.where(game.columns.deer > 0)
-		result_proxy = connection.execute(stmt)
-		results = result_proxy.fetchall()
-		df = pd.DataFrame(results, columns=columns)
-		df = df.apply(pd.to_numeric, errors='ignore')
-		plot_data.lunar_plot(df)
+	while True:
+		print('CHOICES:')
+		print('(0) exit')
+		print('(1) View deer by moonphase')
+		print('(2) View deer by temperature')
+		choice = input('What do you want to do? ').strip()
 
-	if choice == '2':
-		stmt = stmt.where(game.columns.deer > 0)
-		result_proxy = connection.execute(stmt)
-		results = result_proxy.fetchall()
-		df = pd.DataFrame(results, columns=columns)
-		df = df.apply(pd.to_numeric, errors='ignore')
-		plot_data.temp_plot(df)
+		if choice == '0':
+			break
+
+		if choice == '1':
+			stmt = stmt.where(game.columns.deer > 0)
+			result_proxy = connection.execute(stmt)
+			results = result_proxy.fetchall()
+			df = pd.DataFrame(results, columns=columns)
+			df = df.apply(pd.to_numeric, errors='ignore')
+			plot_data.lunar_plot(df)
+
+		if choice == '2':
+			stmt = stmt.where(game.columns.deer > 0)
+			result_proxy = connection.execute(stmt)
+			results = result_proxy.fetchall()
+			df = pd.DataFrame(results, columns=columns)
+			df = df.apply(pd.to_numeric, errors='ignore')
+			plot_data.temp_plot(df)
 
 
 	# plot_data.hogs_stand_time_histogram(df)
