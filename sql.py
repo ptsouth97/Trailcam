193947128c6ea783753b4a115b1d553a8a714608
ptsouth97 <ptsouth97@gmail.com>
2018-09-16 13:11:42 -0400

    fixed bug stopping .csv from being added to db from app

--- sql.py
+++ sql.py
@@ -7,15 +7,15 @@ import pandas as pd
 def main():
 	''' main function for testing and manual entries'''
 
-	load_csv()
+	file_name = input('What is the name of the .csv file to be added to the database? ').lstrip()
+	load_csv(file_name)
 	
 
-def load_csv():
+def load_csv(fname):
 	''' loads the csv file into the database'''
 
-	file_name = input('What is the name of the .csv file to be added to the database? ').lstrip()
 	engine = create_engine('sqlite:///database.db')
-	csv_file = pd.read_csv(file_name)
+	csv_file = pd.read_csv(fname)
 	csv_file.to_sql(name='game', if_exists='append', con=engine, index=False)
 
 
